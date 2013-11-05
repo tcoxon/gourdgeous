a5a4353323e0fe5aed5fd276acef6e55b31b675c
Tom Coxon <tom.c.coxon@googlemail.com>
2013-11-05 23:13:38 +0000

    Replace test text with lorem ipsum.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -41,7 +41,7 @@ class PlayState extends FlxState {
         pumpkin.x = pumpkin.y = 48;
         add(pumpkin);
 
-        var textbox = new TextBox("This is a test; please wait for the signal to ignore!\nOr just don't...\n\nWhatever.");
+        var textbox = new TextBox("Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.\n\nDuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.");
         add(textbox);
     }
 }
